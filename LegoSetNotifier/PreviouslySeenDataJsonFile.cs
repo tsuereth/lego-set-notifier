b39c07b3a1b770cef0de3785ea25e7b50e508763
tsuereth <tsuereth@tsuereth.com>
2024-05-21 07:37:22 -0700

    I missed the right mode: 'Create'

--- LegoSetNotifier/PreviouslySeenDataJsonFile.cs
+++ LegoSetNotifier/PreviouslySeenDataJsonFile.cs
@@ -63,7 +63,7 @@ namespace LegoSetNotifier
         public async Task UpdateSetsAsync(DateTimeOffset updatedTime, Dictionary<string, RebrickableData.LegoSet> legoSets)
         {
             // Ensure the filepath is writable before updating the current data.
-            using (var fileStream = File.Open(this.FilePath, FileMode.Truncate, FileAccess.Write))
+            using (var fileStream = File.Open(this.FilePath, FileMode.Create, FileAccess.Write))
             {
                 this.UpdatedTime = updatedTime;
                 this.Sets = legoSets;
