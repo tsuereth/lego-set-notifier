9584fad254438d35ea8ba4d06b1ea68265f1e5d5
tsuereth <tsuereth@tsuereth.com>
2024-06-21 06:20:28 -0700

    Yet to be exercised, but it looks like the valid "types" of apprise notification are... - info - success - warning - failure
    (from `class NotifyType` at https://github.com/caronc/apprise/blob/master/apprise/common.py )

--- LegoSetNotifier/AppriseNotifier.cs
+++ LegoSetNotifier/AppriseNotifier.cs
@@ -64,7 +64,7 @@ namespace LegoSetNotifier
         {
             var apprisePayloadObject = new JsonObject()
             {
-                ["type"] = "error",
+                ["type"] = "failure",
                 ["format"] = "text",
                 ["title"] = message,
                 ["body"] = $"{message}\n\n{ex}",
